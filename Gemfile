3225eba29a7ac6f417655ced218ee04c8c35f1a2
Timchen7 <timchen7@yahoo.com>
2017-03-14 02:07:43 +0000

    upload to AWS bucket ok

--- Gemfile
+++ Gemfile
@@ -67,7 +67,8 @@ end
 
 #------------- image management -----------
 gem 'carrierwave'
-gem 'mini_magick', '~> 3.5.0'
+# gem 'mini_magick', '~> 3.5.0'
+gem 'mini_magick', '~> 4.1.0'
 gem 'fog'
 gem 'figaro'
 gem 'unf'
--- README.rb
+++ README.rb
@@ -1,5 +1,6 @@
 https://www.udemy.com/build-a-photo-blog-with-ruby-on-rails/learn/v4/t/lecture/2605446?start=0
 Build a Photo Blog with Ruby on Rails
+https://github.com/jordanhudgens/photoflash
 
 rails new photoflash
 rake db:create
@@ -25,3 +26,37 @@ CarrierWave.configure do |config|
   config.fog_directory = ENV['AWS_BUCKET']
   config.fog_public    = false
 end
+
+PROBLEM #1: 
+Errno::ENOENT in PostsController#create
+No such file or directory - identify
+FIXED: 
+I hadn't installed ImageMagick into my Cloud9 Rails IDE workstation! 
+ran 'sudo apt-get update' in the workstation terminal, 
+then 'sudo apt-get install ImageMagick' 
+
+PROBLEM #2:
+Excon::Error::BadRequest in PostsController#create 
+The authorization header is malformed; the region 'us-east-1' is wrong; 
+expecting 'us-west-1'
+FIXED:
+by creating new Bucket ucast-development1 selecting region us-east-1 
+(https://console.aws.amazon.com/s3/home?region=us-east-1)
+
+PROBLEM #3:
+If git push code to GitHub and immediately got yelled at by AWS for exposing my secret keys
+do you have any strategies for 'masking' the keys in source control? 
+I need to pass the yml file to Heroku to run my application live 
+and I can't do that without having that file in github. Thanks
+SOLUTION:
+Yes, you'll need to add your application.yml file to your gitignore file. 
+If you ran the figaro generator it should have done that for you automatically. 
+You can take a look at the source code for the project to see 
+what your gitignore file should look like:
+https://github.com/jordanhudgens/photoflash/blob/master/.gitignore
+Check /vendor/.gitignore file should contained following line
+# Ignore application configuration
+/config/application.yml
+
+next:#4-15:
+Rendering images from the server onto the web application
